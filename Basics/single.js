f8e59030355931808d041fe2badffd6bdb2f1b26
ThePythonGuyy <thepythonguy@outlook.com>
2023-04-11 17:47:52 +0530

    single reducer

---
+++ Basics/single.js
@@ -0,0 +1,40 @@
+// Single Reducer
+
+const redux = require('redux')
+const createStore = redux.legacy_createStore
+
+const BUY_CAKE = 'BUY_CAKE'
+
+
+//ACTION
+function buyCake() {
+    return{
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+
+// (previousState, action) => newState
+
+const initialState = {
+    numofCakes: 10,
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE : return {
+            ...state,
+            numofCakes: state.numofCakes - 1
+        } 
+
+        default: return state
+    }
+}
+
+const store = createStore(reducer);
+console.log('Initial state', store.getState())
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
